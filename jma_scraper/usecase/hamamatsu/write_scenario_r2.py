543d33cd1358a2a518da5c560eb3aa738f74941b
t-edzuka <t.edzuka@happy-quality.jp>
2023-01-11 13:34:13 +0900

    bucket_nameの名前指定必須に.

--- jma_scraper/usecase/hamamatsu/write_scenario_r2.py
+++ jma_scraper/usecase/hamamatsu/write_scenario_r2.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from typing import Union
 
 from botocore.exceptions import EndpointConnectionError
-from pydantic import HttpUrl
+from pydantic import HttpUrl, validate_arguments
 from sqlmodel import Session, create_engine
 
 from jma_scraper.core.location_spec import HAMAMATSU
@@ -13,10 +13,10 @@ from jma_scraper.infrastracture.db_tables import R2UploadFailed, R2UploadSucceed
 from jma_scraper.infrastracture.localfile import JMA_CSV_DIR, RESOURCE_ROOT
 from jma_scraper.infrastracture.r2 import LocalToR2Writer, R2Conf
 from jma_scraper.infrastracture.sqlite_starter import (
-    DB_FILE_DIR,
     create_db_and_tables,
     create_session,
     create_sql_url,
+    DB_PATH
 )
 
 
@@ -33,7 +33,6 @@ def write_from_local_to_r2(
     )
     if dst is None:
         dst = f"{RESOURCE_ROOT.name}/{JMA_CSV_DIR.name}/{src_values.format()}.csv"
-        assert "__data__/jma_csv/2022-01-01__hamamatsu__every_10_minutes.csv" == dst
     r2_writer = LocalToR2Writer(src_values, r2_conf)
     dst_url = HttpUrl(r2_conf.create_dst_url(dst), scheme="https")
     try:
@@ -56,19 +55,16 @@ def write_from_local_to_r2(
     session.close()
 
 
-if __name__ == "__main__":
-    db_log_file = (
-        DB_FILE_DIR / f"{datetime.now().strftime('%Y-%m-%d__%H%M%S')}_sqlite.log"
-    )
-    sys.stdout = open(db_log_file, "a")
-    sqlite_url = create_sql_url(f'{RESOURCE_ROOT / "jma_db" / "jma_app.db"}')
+@validate_arguments
+def main(date_: date, src_csv_file: str):
+    sqlite_url = create_sql_url(DB_PATH)
     engine = create_engine(sqlite_url, echo=True)
     create_db_and_tables(engine)
 
     session = create_session(engine)
     write_from_local_to_r2(
-        date(2022, 1, 1),
-        r2_conf=R2Conf(),
-        src=JMA_CSV_DIR / "2022-01-01__hamamatsu__every_10_minutes.csv",
+        date_=date_,
+        r2_conf=R2Conf(BUCKET_NAME="00-hq"),
+        src=JMA_CSV_DIR / src_csv_file,
         session=session,
     )
